ebc4904106f934bb2533eea2f0735268ed00be4b
TJordan <Tjordan7@stu.jsu.edu>
2022-02-22 23:12:57 -0600

    Finish migrate

--- src/TheRenamer.groovy
+++ src/TheRenamer.groovy
@@ -11,6 +11,8 @@ class TimFormatter extends Formatter {
     }
 }
 
+//TODO: trigger on file add
+
 final String TEST_INPUT_PATH = /E:\\Temp\Test/
 final String TEST_OUTPUT_PATH = /E:\\Temp\Test output/
 final String TEST_LOG_PATH = /E:\\testLog.log/
@@ -29,68 +31,38 @@ final String TV_AND_MOVIE_FORMAT = '{plex.tail}'
 final String ANIME_FORMAT = /{n}\{n} - {s00e00} - {t}/
 
 
+//TODO timestamp log
 FileHandler logOutput = new FileHandler(TEST_LOG_PATH, true)
 logOutput.setFormatter(new TimFormatter())
 log.addHandler(logOutput)
 log.info "\n\n\n\n**********Now Looking for Files**********"
 
-
 log.info "\n**********Checking for TV Shows**********"
-[TV_INPUT_PATH as File].eachMediaFolder {
-    rename(folder:it, db:'TheTVDB', output:TV_OUTPUT_PATH, format:TV_AND_MOVIE_FORMAT, strict:false)
-}
-
+process(TV_INPUT_PATH, 'TheTVDB', TV_OUTPUT_PATH, TV_AND_MOVIE_FORMAT, false)
 
 log.info "\n**********Checking for Movies**********"
-[MOVIE_INPUT_PATH as File].eachMediaFolder {
-    rename(folder:it, db:'TheMovieDB', output:MOVIE_OUTPUT_PATH, format:TV_AND_MOVIE_FORMAT, strict:false)
-}
+process(MOVIE_INPUT_PATH, 'TheMovieDB', MOVIE_OUTPUT_PATH, TV_AND_MOVIE_FORMAT, false)
 
 log.info "\n**********Checking for Anime**********"
-[ANIME_INPUT_PATH as File].eachMediaFolder {
-    rename(folder:it, db:'AniDB', output:ANIME_OUTPUT_PATH, format:ANIME_FORMAT, strict:false)
-}
+process(ANIME_INPUT_PATH, 'AniDB', ANIME_OUTPUT_PATH, ANIME_FORMAT, false)
 
-log.info "\n**********Cleaning Directories**********"
-def allowedExtensions = ["mp4", "mkv", "avi", "srt"]
-def clean
-clean = {
-
-    if (it.isDirectory()) {
-        if (it.listFiles().length == 0) {it.delete()}
-        else {
-            File[] files = it.listFiles()
-            files.each {
-                String name = it.getName()
-                if (it.isDirectory()) {
-                    clean(it)
-                } else if (!allowedExtensions.any {name.contains(it)}) {
-                    it.delete()
-                }
-            }
-            if (it.listFiles().length == 0) {it.delete()}
-        }
-    } else {
-        String fileName = it.getName()
-        int i = fileName.lastIndexOf(".")
-        String fileType = fileName.substring(i)
-
-        if (!fileType.equals('mkv') && !fileType.equals('avi')) {
-            it.delete()
-        }
-    }
-}
-
-def cleanDir = {
-    File dir = new File(it)
-    File[] files = dir.listFiles()
-
-    files.each {
-        clean(it)
-    }
-
-}
-
-[TV_INPUT_PATH, MOVIE_INPUT_PATH, ANIME_INPUT_PATH].each{cleanDir(TEST_INPUT_PATH)}
+//TODO email/text if dir not empty?
+//TODO email/text if multiple options? (encompassed by above?)
 
 log.info("**********now Exiting**********")
+
+
+private void process(String inPath, String dbIn, String outPath, String formatIn, boolean strictIn) {
+    def allowedExtensions = ["mp4", "mkv", "avi", "srt"]
+    File dir = new File(inPath)
+    rename(folder:inPath, db:dbIn, output:outPath, format:formatIn, strict:strictIn)
+    File[] leftovers = dir.listFiles()
+
+    leftovers.each {
+        String name = it.getName()
+        if (it.isDirectory()) { process(it.toString(), dbIn, outPath, formatIn, strictIn) }
+        else if (!allowedExtensions.any {name.contains(it)}) { it.delete() }
+    }
+
+    if (dir.listFiles().length == 0) { dir.delete() }
+}
