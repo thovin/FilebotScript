d78e2fa8269f9d56f7ab9d3dd0ab14e8688bcb2a
TJordan <Tjordan7@stu.jsu.edu>
2022-03-04 15:20:07 -0600

    add timestamps and change anime format, begin add global variables

--- src/Test.groovy
+++ src/Test.groovy
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat
 import java.util.logging.FileHandler
 import java.util.logging.Formatter
 import java.util.logging.LogRecord
@@ -11,7 +12,6 @@ class TimFormatter extends Formatter {
     }
 }
 
-//TODO: trigger on file add
 
 final String TEST_INPUT_PATH = /E:\\Temp\Test/
 final String TEST_OUTPUT_PATH = /E:\\Temp\Test output/
@@ -28,28 +28,35 @@ final String MOVIE_OUTPUT_PATH = /\\mixvault\Plex\Data\Movies/
 final String ANIME_OUTPUT_PATH = /\\mixvault\Plex\Data\Anime/
 
 final String TV_AND_MOVIE_FORMAT = '{plex.tail}'
-final String ANIME_FORMAT = /{n}\{n} - {s00e00} - {t}/
+//final String ANIME_FORMAT = /{n}\{n} - {s00e00} - {t}/
+final String ANIME_FORMAT = /{n.sortName('$2, $1')}\{n} - {s00e00} - {t}/
 
+final SimpleDateFormat timeFormat = new SimpleDateFormat("M/d/yy HH:mm:ss")
+def root = ""
+def anime = false
 
-//TODO timestamp log
 FileHandler logOutput = new FileHandler(TEST_LOG_PATH, true)
 logOutput.setFormatter(new TimFormatter())
 log.addHandler(logOutput)
-log.info "\n\n\n\n**********Now Looking for Files**********"
+log.info "\n\n\n\n**********[" + timeFormat.format(new Date()) + "] Looking for Files**********"
 
 log.info "\n**********Checking for TV Shows**********"
-process(TEST_INPUT_PATH, 'TheTVDB', TEST_OUTPUT_PATH, TV_AND_MOVIE_FORMAT, false)
+root = TEST_INPUT_PATH
+//process(root, 'TheTVDB', TEST_OUTPUT_PATH, TV_AND_MOVIE_FORMAT, false, root)
 
 log.info "\n**********Checking for Movies**********"
-process(TEST_INPUT_PATH, 'TheMovieDB', TEST_OUTPUT_PATH, TV_AND_MOVIE_FORMAT, false)
+root = TEST_INPUT_PATH
+//process(root, 'TheMovieDB', TEST_OUTPUT_PATH, TV_AND_MOVIE_FORMAT, false, root)
 
 log.info "\n**********Checking for Anime**********"
-process(TEST_INPUT_PATH, 'AniDB', TEST_OUTPUT_PATH, ANIME_FORMAT, false)
+root = TEST_INPUT_PATH
+anime = true
+process(root, 'AniDB', TEST_OUTPUT_PATH, ANIME_FORMAT, false, root)
 
 //TODO email/text if dir not empty?
 //TODO email/text if multiple options? (encompassed by above?)
 
-log.info("**********now Exiting**********")
+log.info("**********[" + timeFormat.format(new Date()) + "] Exiting**********")
 
 
 
@@ -57,7 +64,10 @@ private void process(String inPath, String dbIn, String outPath, String formatIn
     def allowedExtensions = ["mp4", "mkv", "avi", "srt"]
     File dir = new File(inPath)
     if (!inPath.equals(root)) {
-        rename(folder: inPath, db: dbIn, output: outPath, format: formatIn, strict: strictIn)
+        if (anime) {
+            rename(folder: inPath, db: dbIn, output: outPath, format: formatIn, strict: strictIn, )
+        }
+
 
         File[] leftovers = dir.listFiles()
 
